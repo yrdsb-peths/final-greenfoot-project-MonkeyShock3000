e1058d1b94d58a009bdba5fdea13f35212424f5a
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-05-23 10:05:41 -0400

    Added Background

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The world.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Aaran 
+ * @version May
  */
 public class MyWorld extends World
 {

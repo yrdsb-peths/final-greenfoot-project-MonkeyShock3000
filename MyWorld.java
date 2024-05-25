d41718e30fd005fd1bfa1ddae862e10b9e8f5555
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-05-24 22:17:42 -0400

    Created Objects
    - Creating Pong game
    - Created two players and the ball

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,17 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        //Create the ball object 
+        Ball ball = new Ball();
+        addObject(ball, getWidth()/2, getHeight()/2);
+        
+        //Create Player 1 object
+        Blue blue = new Blue();
+        addObject(blue, 20, getHeight()/2);
+        
+        //Create Player 2 object
+        Red red = new Red();
+        addObject(red, getWidth() - 20, getHeight()/2);
     }
 }
